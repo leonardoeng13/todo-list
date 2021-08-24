09053737d85157deaf0e13af7d060a3b56ff4d15
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-08-24 20:22:21 -0300

    Linters Fix and README file creation

--- README.md
+++ README.md
@@ -10,66 +10,44 @@ Additional description about the project and its features.
 
 ## Built With
 
-- Major languages
-- Frameworks
-- Technologies used
+- Js
+- Html
+- Css
 
 ## Live Demo
 
-[Live Demo Link](https://livedemo.com)
+[Live Demo Link](https://leonardoeng13.github.io/todo-list/)
 
 
 ## Getting Started
 
-**This is an example of how you may give instructions on setting up your project locally.**
-**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
-
-
 To get a local copy up and running follow these simple example steps.
 
-### Prerequisites
+###Prerequisites
 
-### Setup
-
-### Install
-
-### Usage
-
-### Run tests
-
-### Deployment
-
+- Git/GitHub;
+- Optional ( IDE installed in your machine ) - recommended IDE: Vscode;
+- Node installed locally;
 
 
 ## Authors
 
 👤 **Author1**
 
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@leonardoeng13](https://github.com/leonardoeng13)
+- Twitter: [@Leo Andrade](https://twitter.com/deandrede_leo)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/leonardodeandrade/)
 
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
 
 ## 🤝 Contributing
 
 Contributions, issues, and feature requests are welcome!
 
-Feel free to check the [issues page](../../issues/).
+Feel free to check the [issues page](https://github.com/leonardoeng13/todo-list/issues).
 
 ## Show your support
 
 Give a ⭐️ if you like this project!
-
-## Acknowledgments
-
-- Hat tip to anyone whose code was used
-- Inspiration
-- etc
 
 ## 📝 License
 
--- src/main.js
+++ src/main.js
@@ -1,125 +1,118 @@
-// Select the Elements
-const clear = document.querySelector(".clear");
-const dateElement = document.getElementById("date");
-const list = document.getElementById("list");
-const input = document.getElementById("input");
+const clear = document.querySelector('.clear');
+const dateElement = document.getElementById('date');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
 
-// Classes names
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH = "lineThrough";
+const CHECK = 'fa-check-circle';
+const UNCHECK = 'fa-circle-thin';
+const LINE_THROUGH = 'lineThrough';
 
-// Variables
-let LIST, id;
+let LIST; let
+  id;
 
-// get item from localstorage
-let data = localStorage.getItem("TODO");
+const data = localStorage.getItem('TODO');
 
-// check if data is not empty
-if(data){
-    LIST = JSON.parse(data);
-    id = LIST.length;
-    loadList(LIST);
-}else{
-    // if data isn't empty
-    LIST = [];
-    id = 0;
-}
-
-// load items to the user's interface
-function loadList(array){
-    array.forEach(function(item){
-        addToDo(item.name, item.id, item.done, item.trash);
-    });
-}
-
-// clear the local storage
-clear.addEventListener("click", function(){
-    localStorage.clear();
-    location.reload();
+clear.addEventListener('click', () => {
+  localStorage.clear();
+  window.location.reload();
 });
 
-// Show todays date
-const options = {weekday : "long", month:"short", day:"numeric"};
+const options = { weekday: 'long', month: 'short', day: 'numeric' };
 const today = new Date();
 
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+dateElement.innerHTML = today.toLocaleDateString('en-US', options);
 
 // add to do function
 
-function addToDo(toDo, id, done, trash){
-    
-    if(trash){ return; }
-    
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
-    
-    const item = `<li class="item">
+function addToDo(toDo, id, done, trash) {
+  if (trash) { return; }
+
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : '';
+
+  const item = `<li class="item">
                     <i class="fa ${DONE} co" job="complete" id="${id}"></i>
                     <p class="text ${LINE}">${toDo}</p>
                     <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
                   </li>
                 `;
-    
-    const position = "beforeend";
-    
-    list.insertAdjacentHTML(position, item);
+
+  const position = 'beforeend';
+
+  list.insertAdjacentHTML(position, item);
+}
+
+// load items to the user's interface
+function loadList(array) {
+  array.forEach((item) => {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+}
+
+// check if data is not empty
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadList(LIST);
+} else {
+  // if data isn't empty
+  LIST = [];
+  id = 0;
 }
 
 // add an item to the list user the enter key
-document.addEventListener("keyup",function(even){
-    if(event.keyCode == 13){
-        const toDo = input.value;
-        
-        // if the input isn't empty
-        if(toDo){
-            addToDo(toDo, id, false, false);
-            
-            LIST.push({
-                name : toDo,
-                id : id,
-                done : false,
-                trash : false
-            });
-            
-            // add item to localstorage ( this code must be added where the LIST array is updated)
-            localStorage.setItem("TODO", JSON.stringify(LIST));
-            
-            id++;
-        }
-        input.value = "";
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    const toDo = input.value;
+
+    // if the input isn't empty
+    if (toDo) {
+      addToDo(toDo, id, false, false);
+
+      LIST.push({
+        name: toDo,
+        id,
+        done: false,
+        trash: false,
+      });
+
+      // add item to localstorage ( this code must be added where the LIST array is updated)
+      localStorage.setItem('TODO', JSON.stringify(LIST));
+
+      id += id + 1;
     }
+    input.value = '';
+  }
 });
 
-
 // complete to do
-function completeToDo(element){
-    element.classList.toggle(CHECK);
-    element.classList.toggle(UNCHECK);
-    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
-    
-    LIST[element.id].done = LIST[element.id].done ? false : true;
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = !LIST[element.id].done;
 }
 
 // remove to do
-function removeToDo(element){
-    element.parentNode.parentNode.removeChild(element.parentNode);
-    
-    LIST[element.id].trash = true;
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+
+  LIST[element.id].trash = true;
 }
 
 // target the items created dynamically
 
-list.addEventListener("click", function(event){
-    const element = event.target; // return the clicked element inside list
-    const elementJob = element.attributes.job.value; // complete or delete
-    
-    if(elementJob == "complete"){
-        completeToDo(element);
-    }else if(elementJob == "delete"){
-        removeToDo(element);
-    }
-    
-    // add item to localstorage ( this code must be added where the LIST array is updated)
-    localStorage.setItem("TODO", JSON.stringify(LIST));
+list.addEventListener('click', (event) => {
+  const element = event.target; // return the clicked element inside list
+  const elementJob = element.attributes.job.value; // complete or delete
+
+  if (elementJob === 'complete') {
+    completeToDo(element);
+  } else if (elementJob === 'delete') {
+    removeToDo(element);
+  }
+
+  // add item to localstorage ( this code must be added where the LIST array is updated)
+  localStorage.setItem('TODO', JSON.stringify(LIST));
 });
